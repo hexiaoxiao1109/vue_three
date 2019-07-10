1bc4cacdfee06649a55ab114ee21657edf6dec7e
hexiaoxiao1109 <51736769+hexiaoxiao1109@users.noreply.github.com>
2019-07-10 09:13:55 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,12 +5,12 @@ import qs from 'qs'
 
 //3.定义接口 并 导出
 
-export const sListApi = (apiParams) => {
-    return axios({
-        method:'post',
-        url: '/order',
-        data: qs.stringify(apiParams) 
-    }).then(res => res.data)
-}
+// export const sListApi = (apiParams) => {
+//     return axios({
+//         method:'post',
+//         url: '/order',
+//         data: qs.stringify(apiParams) 
+//     }).then(res => res.data)
+// }
 
 
