493b3309c9f99059cd9f6dfee7d347fd14497d46
““hu5210“ <280383929@qq.com>
2019-07-10 19:47:08 +0800

    一部分

--- src/views/Uselist.vue
+++ src/views/Uselist.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
+    <Input v-model="orderno" placeholder="Enter something..." clearable style="width: 200px" class="mar" />
+     <Button type="primary" shape="circle" icon="ios-search" class="mar" @click="serch"></Button>
     <Table border :columns="columns12" :data="data6">
         <template slot-scope="{ row }" slot="name">
         <strong>{{ row.name }}</strong>
         </template>
         <template slot-scope="{ row, index }" slot="action">
-        <Button type="primary" size="small" style="margin-right: 5px" @click="show(index)">修改</Button>
-        <Button type="error" size="small" @click="remove(index)" v-model="data6">删除</Button>
+        <Button type="primary" size="small" style="margin-right: 5px" @click.native="show(index)">修改</Button>
+        <Button type="error" size="small" @click="remove(index)">删除</Button>
         </template>
     </Table>
-    <Page :total="100" show-sizer />
+    <Page :total="100" show-sizer @on-change="changepage" class=".mar"/>
   </div>
 </template>
 <script>
@@ -20,23 +22,26 @@ export default {
   },
   data() {   
     return {
+      pageno:1,
+      orderno:"",
+       value: '',
         data6: [],
       columns12: [
+         {
+          title: "编号",
+          key: "id"
+        },
         {
           title: "订单号",
           key: "order_no"
         },
         {
-          title: "商品",
-          key: "goods_name"
-        },
-        {
           title: "数量",
-          key: "goods_number"
+          key: "total_num"
         },
         {
           title: "价格",
-          key: `goods_price`
+          key: `total_price`
         },
         {
           title: "更改",
@@ -48,10 +53,20 @@ export default {
     };
   },
   methods: {
+    //分页
+    //制动有一个形参代表第几页
+    changepage(pageno){
+   this.pageno=pageno,
+   this.initdata()
+    },
+    //搜索
+    serch(){
+     this.initdata()
+    },
     show(index) {
       this.$Modal.info({
         title: "User Info",
-        content: `价格：<input value='${this.data6[index].goods_price}' style='margin-bottom:20px'></input><br>数量：<input value='${this.data6[index].goods_number}'></input>`
+        content: `价格：<input  style='margin-bottom:20px'></input><br>数量：<input ></input>`
       });
     },
     remove(index) {
@@ -59,11 +74,17 @@ export default {
     },
   initdata() {
     sListApi({
+      orderno:this.orderno,
+       pageno:this.pageno,   
       }).then(res => {
-          console.log(res)
         this.data6=(res.data);
       })
   },
 }
 }
 </script>
+<style scoped>
+.mar{
+    margin: 10px 0 10px 5px
+}
+</style>
